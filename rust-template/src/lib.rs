774d52fce08874d84211fae6c3f9e34fbf090a76
Ivan <rexx.magtar@gmail.com>
2022-05-29 21:28:43 +0000

    ready for frontend

--- rust-template/src/lib.rs
+++ rust-template/src/lib.rs
@@ -1,8 +1,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen};
 
-const PUZZLE_NUMBER: u8 = 1;
-
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Contract {
@@ -11,19 +9,32 @@ pub struct Contract {
 
 #[near_bindgen]
 impl Contract {
-    pub fn get_puzzle_number(&self) -> u8 {
-        PUZZLE_NUMBER
+
+    #[init]
+    pub fn new(solution: String) -> Self {
+        Self {
+            crossword_solution: solution,
+        }
     }
 
-    pub fn set_solution(&mut self, solution: String) {
+     fn set_solution(&mut self, solution: String) {
         self.crossword_solution = solution;
     }
 
-    pub fn guess_solution(&mut self, solution: String) {
-        if solution == self.crossword_solution {
-            env::log_str("You guessed right!")
+    pub fn get_solution(&self) -> String {
+        self.crossword_solution.clone()
+    }
+    
+    pub fn guess_solution(&mut self, solution: String) -> bool {
+        let hashed_input = env::sha256(solution.as_bytes());
+        let hashed_input_hex = hex::encode(&hashed_input);
+    
+        if hashed_input_hex == self.crossword_solution {
+            env::log_str("You guessed right!");
+            true
         } else {
-            env::log_str("Try again.")
+            env::log_str("Try again.");
+            false
         }
     }
 }
