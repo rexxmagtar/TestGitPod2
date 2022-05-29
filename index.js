b87acb601758a14909c4eedb18d2141b4acc3d1d
Ivan <rexx.magtar@gmail.com>
2022-05-29 11:02:12 +0000

    server setup

---
+++ index.js
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Super cat</title>
+    </head>
+    <body>
+        <p>Super cat This is an example of a simple HTML page with one paragraph.</p>
+    </body>
+</html>
