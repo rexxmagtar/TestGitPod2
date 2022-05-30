9cb16c461f7618ab122619ef6b4be0f2227a05fa
Ivan <rexx.magtar@gmail.com>
2022-05-30 00:37:36 +0000

    front code intergration with smart contarct began

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import * as nearAPI from 'near-api-js';
 
-function App() {
+
+// Our API could be improved here :)
+// See: https://github.com/near/near-api-js/issues/612
+async function viewMethodOnContract() {
+  const provider = new nearAPI.providers.JsonRpcProvider('https://rpc.testnet.near.org');
+  const rawResult = await provider.query(`call/crossword.friend.testnet/get_solution`, 'AQ4'); // Base 58 of '{}'
+  return JSON.parse(rawResult.result.map((x) => String.fromCharCode(x)).join(''));
+}
+
+async function initCrossword() {
+  const solutionHash = await viewMethodOnContract();
+  return solutionHash ;
+}
+
+async function App() {
+  var hashResult = await initCrossword();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Coded hash = {hashResult}
         </p>
         <a
           className="App-link"
